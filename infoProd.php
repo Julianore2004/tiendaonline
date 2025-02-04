3f8ba0fe7895ded8fcd593dd9479014b5196e63a
Julianore2004 <julianore79@gmail.com>
2025-02-03 19:21:22 -0500

    UPDATE

--- infoProd.php
+++ infoProd.php
@@ -54,8 +54,6 @@ include './library/consulSQL.php';
         .product-image img {
             max-width: 100%;
             height: auto;
-            border-radius: 10px;
-            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
         }
 
         .product-info h2 {
